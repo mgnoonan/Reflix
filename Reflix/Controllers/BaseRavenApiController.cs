eaff063d8a12249a14781c8e36c524bb9895a12a
Matthew Noonan <noonanconsulting@gmail.com>
2013-05-13 22:31:06 -0400

    Added WebApi support

---
+++ Reflix/Controllers/BaseRavenApiController.cs
@@ -0,0 +1,37 @@
+﻿using Raven.Client;
+using Raven.Client.Embedded;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Mvc;
+
+namespace Reflix.Controllers
+{
+    public abstract class BaseRavenApiController : ApiController
+    {
+        public IDocumentSession RavenSession { get; protected set; }
+
+        protected override void Initialize(System.Web.Http.Controllers.HttpControllerContext controllerContext)
+        {
+            base.Initialize(controllerContext);
+            if (RavenSession == null)
+                RavenSession = MvcApplication._store.OpenSession();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            using (RavenSession)
+            {
+                if (RavenSession != null)
+                    RavenSession.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Reflix/Controllers/TitleController.cs
@@ -0,0 +1,47 @@
+﻿using Reflix.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Reflix.Controllers
+{
+    public class TitleController : BaseRavenApiController
+    {
+        // GET api/title
+        public IEnumerable<TitleViewModel> Get()
+        {
+            var query = from title in this.RavenSession.Query<TitleViewModel>()
+                        select title;
+
+            return query.AsEnumerable();
+        }
+
+        // GET api/title/5
+        public TitleViewModel Get(string id)
+        {
+            var query = from title in this.RavenSession.Query<TitleViewModel>()
+                        where title.Title.Id == id
+                        select title;
+
+            return query.SingleOrDefault();
+        }
+
+        //// POST api/title
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT api/title/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE api/title/5
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
