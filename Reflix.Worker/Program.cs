a069380448b89b78e3cb2c760fdb28841eb6d7ce
mgnoonan <noonanconsulting@gmail.com>
2013-12-02 00:12:56 -0500

    Uncomment the Netflix parser, which was accidentally checked in

--- Reflix.Worker/Program.cs
+++ Reflix.Worker/Program.cs
@@ -31,7 +31,7 @@ namespace Reflix.Worker
 
                 DateTime targetDate = Utils.CalculateStartDate();
                 var parsers = new List<ICustomSiteParser>();
-                //parsers.Add(new NetflixSiteParser("http://rss.netflix.com/NewReleasesRSS", targetDate, "Netflix"));
+                parsers.Add(new NetflixSiteParser("http://rss.netflix.com/NewReleasesRSS", targetDate, "Netflix"));
                 parsers.Add(new ComingSoonNetSiteParser("http://www.commingsoon.net/dvd", targetDate, "ComingSoon.net"));
                 parsers.Add(new MoviesDotComSiteParser("http://www.movies.com/rss-feeds/new-on-dvd-rss", targetDate, "Movies.com"));
                 //parsers.Add(new BlockbusterSiteParser("http://www.blockbuster.com/rss/newRelease", targetDate, "Blockbuster"));
